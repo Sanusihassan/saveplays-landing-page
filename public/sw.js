8d30b05778c6ec12849e67cc200eb8eae01011a2
Sanusi Hassan <sanusihassan122@gmail.com>
2025-01-05 13:03:45 +0200

    made the site pwa

---
+++ public/sw.js
@@ -0,0 +1,22 @@
+// sw.js
+const CACHE_NAME = 'saveplays-v1';
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                // Add other important assets
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
